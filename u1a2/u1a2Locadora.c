cb930d11af2b93167c62ed2f8f0a929c11103bcf
ferreiracassior <cassio.ribeiro.ferreira@outlook.com>
2022-09-15 23:09:57 -0300

    commit u1a2

--- u1a2/u1a2Locadora.c
+++ u1a2/u1a2Locadora.c
@@ -1,20 +1,3 @@
-/*  
-    Até então, você já construiu um algoritmo capaz de receber como entradas a idade do cliente 
-e a classificação indicativa do filme que ele pretende locar e, logo após, mostrar na tela um dos 
-possíveis resultados: “Este filme não é indicado para sua faixa etária” ou “Este filme é indicado para sua faixa etária”. 
-    Agora é hora de tirar essa ideia do papel e colocar para funcionar em um computador. Contudo, você ainda não dispõe de 
-todos os conhecimentos necessários para implementar essa solução na linguagem C.
-Por isso, seu chefe lhe passou outra tarefa. Ele quer que seu programa seja capaz de ler a idade e o nome do cliente, 
-bem como a classificação do filme que ele deseja locar. Posteriormente, seu programa deve imprimir todas essas informações na tela, 
-conforme o padrão a seguir:
-
-    Cliente: José das Couves
-    Idade: 18 anos
-    Classificação do filme: 12 anos
-
-    Seu chefe, que também é analista de sistemas, informou que você deve utilizar os conceitos de struct, variáveis e 
-constantes para resolver esse problema.
-*/
 
 #include <stdio.h>
 
